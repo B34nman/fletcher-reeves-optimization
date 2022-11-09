48b68872c40c1155408cd014f608aafd78aeb7d9
Andy <61066242+B34nman@users.noreply.github.com>
2022-11-09 12:48:20 -0800

    updated file

--- Homework3.m
+++ Homework3.m
@@ -1,23 +1,26 @@
-warning off;
+%warning off;
+format long;
 
 x = [-1.2;1];
 A = 100;
 
+fletcher_reeves(x, A);
+
 function fletcher_reeves(x, A)
 
-    storage = zeros(2, 6000); %storage for path plot
+    %storage = zeros(2, 6000); %storage for path plot
     c = 0.01;
     rho = .5;
     f=func(x, A);
     g=grad(x, A);
+    pk = -g; % steepest descent direction
     k = 0; % k = # iterations
     funcEval=1;	% funcEval = # function eval.	
 
 
-    while 
+    while g ~= 0
 
         %this is our step size calculation
-        pk = -g; % steepest descent direction
         a = 1;
         newf = func(x + a*pk, A);
         funcEval = funcEval+1;
@@ -27,8 +30,31 @@ function fletcher_reeves(x, A)
             funcEval = funcEval+1;
         end
 
+        x = x + a*pk; % gradient descent
+        gNew=grad(x, A); %gk+1 = grad(f(x+1))
+        
+        %beta evaluation
+        top = dot(transpose(gNew), gNew);
+        bot = dot(transpose(g), g);
+        beta = top/bot;
+
+        %update g value
+        g = gNew;
+
+        %new pk value
+        pk = -1*g + beta*pk;
+        if (dot(transpose(pk), g) > 0)
+            pk = -g;
+        end
+
+        %iterations
+        k = k + 1;
+
+        
+
     end
     
+    disp(x);
 
 end
 
@@ -40,11 +66,4 @@ function y = grad(x, A)
     y(1) = A*(2*(x(1)^2-x(2))*2*x(1)) + 2*(x(1)-1);
     y(2) = A*(-2*(x(1)^2-x(2)));
     y = y';
-end
-
-function y = hessian(x, A)
-    y(1,1) = (12*A) * x(1)^2 - (4*A) * x(2) + 2;
-    y(1,2) = -(4*A) * x(1);
-    y(2,1) = -(4*A) * x(1);
-    y(2,2) = (2*A);
 end
